da97caf0c00945574272630e99b8e5b64efae8d3
Michele Locati <michele@locati.it>
2015-09-01 10:59:14 +0200

    Use temporary directory

--- lib/compile.rb
+++ lib/compile.rb
@@ -1,3 +1,4 @@
+require "fileutils"
 require "asciidoctor"
 
 $rootDir = File.expand_path(File.dirname(File.dirname(__FILE__)))
@@ -15,14 +16,14 @@ def process(which)
 
     print "  - Generating DocBook... "
     docbook = Asciidoctor.convert_file $rootDir + "/" + which + ".adoc", :to_file => false, :header_footer => true, :safe => Asciidoctor::SafeMode::UNSAFE, :backend => "docbook", :attributes => {"numbered" => true}
-    f = File.open($rootDir + "/output/" + which + "-docbook.xml", "wb")
+    f = File.open($rootDir + "/tmp/" + which + "-docbook.xml", "wb")
     f.write(docbook)
     f.close
     print "done.\n"
 
     print "  - Generating chunked html... "
     Dir.mkdir($rootDir + "/output/" + which) unless Dir.exists?($rootDir + "/output/" + which)
-    if system("xsltproc --output " + $rootDir + "/output/" + which + "/myfile.html " + $rootDir + "/lib/html-chunked-parameters.xsl " + $rootDir + "/output/" + which + "-docbook.xml") == false
+    if system("xsltproc --output " + $rootDir + "/output/" + which + "/index.html " + $rootDir + "/lib/html-chunked-parameters.xsl " + $rootDir + "/tmp/" + which + "-docbook.xml") == false
     	raise "xsltproc failed!"
     end
     print "done.\n"
@@ -30,6 +31,8 @@ end
 
 $stdout.sync = true
 
+FileUtils.rm_rf($rootDir + "/tmp")
+Dir.mkdir($rootDir + "/tmp")
 Dir.mkdir($rootDir + "/output") unless Dir.exists?($rootDir + "/output")
 
 process "developers"
