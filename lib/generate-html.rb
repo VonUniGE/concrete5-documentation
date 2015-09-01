24171389e49127106dcc9fc62c8b03c9f32eff5a
Michele Locati <michele@locati.it>
2015-09-01 08:27:13 +0200

    Always use HTTPS for YouTube video URLs

--- lib/generate-html.rb
+++ lib/generate-html.rb
@@ -2,6 +2,7 @@ require "asciidoctor"
 
 def process(which)
     adoc = Asciidoctor.convert_file "../" + which + ".adoc", :to_file => false, :header_footer => true, :safe => Asciidoctor::SafeMode::UNSAFE, :attributes => {"numbered" => true}
+    adoc.gsub!('src="//www.youtube.com/', 'src="https://www.youtube.com/')
     Dir.mkdir("../html") unless Dir.exists?("../html")
     f = File.open("../html/" + which + ".html", "wb")
     f.write(adoc)
