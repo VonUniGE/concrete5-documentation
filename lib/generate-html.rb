e53ceb1d5987028ea1370f2b9a153e37ea5e429f
Michele Locati <michele@locati.it>
2015-09-01 09:02:37 +0200

    Use output for the name of the output folder

--- lib/generate-html.rb
+++ lib/generate-html.rb
@@ -3,11 +3,12 @@ require "asciidoctor"
 def process(which)
     adoc = Asciidoctor.convert_file "../" + which + ".adoc", :to_file => false, :header_footer => true, :safe => Asciidoctor::SafeMode::UNSAFE, :attributes => {"numbered" => true}
     adoc.gsub!('src="//www.youtube.com/', 'src="https://www.youtube.com/')
-    Dir.mkdir("../html") unless Dir.exists?("../html")
-    f = File.open("../html/" + which + ".html", "wb")
+    f = File.open("../output/" + which + ".html", "wb")
     f.write(adoc)
     f.close 
 end
+
+Dir.mkdir("../output") unless Dir.exists?("../output")
 
 process "developers"
 # process "editors"
