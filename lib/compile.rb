52078104e17c5ff082e3d195c73028b79ebea42d
Michele Locati <michele@locati.it>
2015-09-01 10:25:57 +0200

    Start creating chunked html

--- lib/compile.rb
+++ lib/compile.rb
@@ -1,14 +1,36 @@
 require "asciidoctor"
 
+$rootDir = File.expand_path(File.dirname(File.dirname(__FILE__)))
+
 def process(which)
-    adoc = Asciidoctor.convert_file "../" + which + ".adoc", :to_file => false, :header_footer => true, :safe => Asciidoctor::SafeMode::UNSAFE, :attributes => {"numbered" => true}
+    print "Processing " + which + ":\n"
+
+    print "  - Complete HTML... "
+    adoc = Asciidoctor.convert_file $rootDir + "/" + which + ".adoc", :to_file => false, :header_footer => true, :safe => Asciidoctor::SafeMode::UNSAFE, :attributes => {"numbered" => true}
     adoc.gsub!('src="//www.youtube.com/', 'src="https://www.youtube.com/')
-    f = File.open("../output/" + which + ".html", "wb")
+    f = File.open($rootDir + "/output/" + which + ".html", "wb")
     f.write(adoc)
-    f.close 
+    f.close
+    print "done.\n"
+
+    print "  - Generating DocBook... "
+    docbook = Asciidoctor.convert_file $rootDir + "/" + which + ".adoc", :to_file => false, :header_footer => true, :safe => Asciidoctor::SafeMode::UNSAFE, :backend => "docbook", :attributes => {"numbered" => true}
+    f = File.open($rootDir + "/output/" + which + "-docbook.xml", "wb")
+    f.write(docbook)
+    f.close
+    print "done.\n"
+
+    print "  - Generating chunked html... "
+    Dir.mkdir($rootDir + "/output/" + which) unless Dir.exists?($rootDir + "/output/" + which)
+    if system("xsltproc --output " + $rootDir + "/output/" + which + "/myfile.html " + $rootDir + "/lib/html-chunked-parameters.xsl " + $rootDir + "/output/" + which + "-docbook.xml") == false
+    	raise "xsltproc failed!"
+    end
+    print "done.\n"
 end
 
-Dir.mkdir("../output") unless Dir.exists?("../output")
+$stdout.sync = true
+
+Dir.mkdir($rootDir + "/output") unless Dir.exists?($rootDir + "/output")
 
 process "developers"
 # process "editors"
